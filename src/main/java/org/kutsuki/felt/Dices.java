2d21cff19d8dc952bb2f216ed41f57d81961cbd4
XanaduBox <XanaduBox@XanaduBox.home>
2017-01-14 22:36:00 -0500

    Progress

--- src/main/java/org/kutsuki/felt/Dices.java
+++ src/main/java/org/kutsuki/felt/Dices.java
@@ -1,24 +1,45 @@
 package org.kutsuki.felt;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.apache.commons.math3.random.MersenneTwister;
 import org.apache.commons.math3.random.RandomAdaptor;
 
 public class Dices {
+	private int index;
+	private List<Integer> rollList;
 	private Random random;
 
 	public Dices() {
 		this.random = RandomAdaptor.createAdaptor(new MersenneTwister());
 	}
 
-	public int roll() {
-		int dice1 = random.nextInt(6) + 1;
-		int dice2 = random.nextInt(6) + 1;
-		return dice1 + dice2;
+	public Dices(int... rolls) {
+		this.index = 0;
+		this.rollList = new ArrayList<Integer>();
+
+		for (int i = 0; i < rolls.length; i++) {
+			rollList.add(rolls[i]);
+		}
 	}
 
-	public int roll(int dice1, int dice2) {
-		return dice1 + dice2;
+	public int roll() {
+		int roll = 0;
+
+		if (rollList == null) {
+			int dice1 = random.nextInt(6) + 1;
+			int dice2 = random.nextInt(6) + 1;
+			roll = dice1 + dice2;
+		} else {
+			roll = rollList.get(index);
+
+			if (index < rollList.size() - 1) {
+				index++;
+			}
+		}
+
+		return roll;
 	}
 }
--- src/main/java/org/kutsuki/felt/Felt.java
+++ src/main/java/org/kutsuki/felt/Felt.java
@@ -1,18 +1,85 @@
 package org.kutsuki.felt;
 
-public class Felt {
-	private Dices dices;
+import java.math.BigDecimal;
 
-	public Felt() {
-		this.dices = new Dices();
+public class Felt {
+	private static final BigDecimal BET = BigDecimal.ONE;
+	private static final BigDecimal ODDS = new BigDecimal(100);
+
+	private BigDecimal dontPassBankroll;
+	private BigDecimal passBankroll;
+	private Dices dices;
+	private int roll;
+
+	public Felt(Dices dices) {
+		this.dices = dices;
+		init();
+	}
+
+	private void init() {
+		this.dontPassBankroll = BigDecimal.ZERO;
+		this.roll = 0;
+		this.passBankroll = BigDecimal.ZERO;
 	}
 
 	public void run() {
+		// make bet
+		passBankroll = passBankroll.subtract(BET);
+		dontPassBankroll = dontPassBankroll.subtract(BET);
 
+		// come out roll
+		roll = dices.roll();
+		System.out.println(roll);
+
+		if (roll == 7) {
+			passBankroll = passBankroll.add(BET);
+			passBankroll = passBankroll.add(BET);
+		} else if (roll == 11) {
+			passBankroll = passBankroll.add(BET);
+			passBankroll = passBankroll.add(BET);
+		} else if (roll == 2 || roll == 3) {
+			dontPassBankroll = dontPassBankroll.add(BET);
+			dontPassBankroll = dontPassBankroll.add(BET);
+		} else if (roll == 12) {
+			dontPassBankroll = dontPassBankroll.add(BET);
+		} else {
+			int point = roll;
+			roll = dices.roll();
+
+			while (roll != point && roll != 7) {
+				if (roll == 6 || roll == 8) {
+
+				} else if (roll == 5 || roll == 9) {
+
+				} else if (roll == 4 || roll == 10) {
+
+				} else if (roll == 11) {
+
+				} else if (roll == 2 || roll == 3 || roll == 12) {
+
+				}
+
+				roll = dices.roll();
+			}
+
+			if (roll == point) {
+				passBankroll = passBankroll.add(BET);
+				passBankroll = passBankroll.add(BET);
+			} else if (roll == 7) {
+				dontPassBankroll = dontPassBankroll.add(BET);
+				dontPassBankroll = dontPassBankroll.add(BET);
+			} else {
+				throw new IllegalStateException(roll + " ended unexpectedly!");
+			}
+		}
+
+		System.out.println(roll);
+		System.out.println(passBankroll);
+		System.out.println(dontPassBankroll);
 	}
 
 	public static void main(String[] args) {
-		Felt felt = new Felt();
+		Felt felt = new Felt(new Dices());
 		felt.run();
 	}
 }
