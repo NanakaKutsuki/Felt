42952c87325a9dff6804e0eb1eeddb647f76cd01
MatchaGreen <MatchaGreen@192.168.86.88>
2021-01-05 10:55:15 -0500

    Fixing Junits

--- src/test/java/org/kutsuki/felt/DicesTest.java
+++ src/test/java/org/kutsuki/felt/DicesTest.java
@@ -6,39 +6,39 @@ import static org.junit.Assert.assertNotEquals;
 import org.junit.Test;
 
 public class DicesTest {
-	@Test
-	public void testDices() {
-		Dices dices = new Dices();
+    @Test
+    public void testDices() {
+	Dices dices = new Dices();
 
-		for (int expected = 1; expected <= 13; expected++) {
-			int actual = 0;
-			int i = 0;
-			int retries = 10000;
-			while (actual != expected && i < retries) {
-				actual = dices.roll();
-				i++;
-			}
+	for (int expected = 1; expected <= 13; expected++) {
+	    int actual = 0;
+	    int i = 0;
+	    int retries = 10000;
+	    while (actual != expected && i < retries) {
+		actual = dices.roll();
+		i++;
+	    }
 
-			if (expected >= 2 && expected <= 12) {
-				assertEquals("Unable to roll!", expected, actual);
-			} else {
-				assertNotEquals("Roll unexpected!", expected, actual);
-				assertEquals("Tries unused on: " + expected, retries, i);
-			}
-		}
+	    if (expected >= 2 && expected <= 12) {
+		assertEquals("Unable to roll!", expected, actual);
+	    } else {
+		assertNotEquals("Roll unexpected!", expected, actual);
+		assertEquals("Tries unused on: " + expected, retries, i);
+	    }
 	}
+    }
 
-	@Test
-	public void testSetDice() {
-		Dices dices = new Dices(4, 7, 5, 4);
-		assertEquals("Roll Unexpected!", 4, dices.roll());
-		assertEquals("Roll Unexpected!", 7, dices.roll());
-		assertEquals("Roll Unexpected!", 5, dices.roll());
-		assertEquals("Roll Unexpected!", 4, dices.roll());
+    @Test
+    public void testSetDice() {
+	Dices dices = new Dices(4, 7, 5, 4);
+	assertEquals("Roll Unexpected!", 4, dices.roll());
+	assertEquals("Roll Unexpected!", 7, dices.roll());
+	assertEquals("Roll Unexpected!", 5, dices.roll());
+	assertEquals("Roll Unexpected!", 4, dices.roll());
 
-		Dices dices2 = new Dices(4, 4, 4);
-		for (int i = 0; i < 10; i++) {
-			assertEquals("Roll Unexpected!", 4, dices2.roll());
-		}
+	Dices dices2 = new Dices(4, 4, 4);
+	for (int i = 0; i < 10; i++) {
+	    assertEquals("Roll Unexpected!", 4, dices2.roll());
 	}
+    }
 }
